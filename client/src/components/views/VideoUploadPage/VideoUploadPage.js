4c2b4f9ea0362dba2784db8b9f9efdb520f08cf4
jisoo <jsoo744@gmail.com>
2020-10-11 07:58:15 +0900

    [*] Add VideoUploadPage

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd'
+import DropZone from 'react-dropzone'
+
+const { Title } = Typography
+const { TextArea } = Input
+
+function VideoUploadPage() {
+  return (
+    <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
+      <div style={{ textAlign:'center', marginBottom:'2rem' }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+      <Form onSubmit>
+        <div style={{ display:'flex', justifyContent:'space-between' }}>
+          <DropZone
+            onDrop
+            multiple
+            maxSize
+          >
+            {
+              ({ getRootProps, getInputProps }) => (
+                <div style={{ display:'flex', width:'300px', height:'240px', border:'1px solid lightgray', alignItems:'center', justifyContent:'center' }} {...getRootProps()}>
+                  <input {...getInputProps()}/>
+                  <Icon type="plus" style={{ fontSize:'3rem' }}/>
+                </div>
+              )
+            }
+          </DropZone>
+          {/* Thumbnail */}
+          <div>
+            <img src="" alt=""/>
+          </div>
+        </div>
+        <br/>
+        <br/>
+        <label>Title</label>
+        <Input
+          onChange
+          value
+        />
+        <br/>
+        <br/>
+        <label>Description</label>
+        <TextArea
+          onChange
+          value
+        />
+        <br/>
+        <br/>
+        <select onChange>
+          <option key value></option>
+        </select>
+        <br/>
+        <br/>
+        <select onChange>
+          <option key value></option>
+        </select>
+        <br/>
+        <br/>
+        <Button type="primary" size="large" onClick>Submit</Button>
+      </Form>
+    </div>
+  )
+}
+
+export default VideoUploadPage
