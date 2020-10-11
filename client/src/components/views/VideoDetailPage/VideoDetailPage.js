1cc1e45f0092077f3a074ec449cd493d41c491a2
jisoo <jsoo744@gmail.com>
2020-10-12 08:29:21 +0900

    [*] Add video detail page

---
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { Row, Col, List, Avatar } from 'antd'
+import Axios from 'axios'
+
+function VideoDetailPage(props) {
+
+  const [videoDetail, setVideoDetail] = useState([])
+
+  useEffect(() => {
+    let variable = { videoId: props.match.params.videoId }
+    Axios.post('/api/video/getVideoDetail', variable)
+      .then(response => {
+        if (response.data.success) {
+          setVideoDetail(response.data.videoDetail)
+        } else {
+          alert('비디오 정보를 가져오지 못했습니다.')
+        }
+      })
+    
+  }, [])
+
+  if (!videoDetail.writer) {
+    return (<div> L O A D I N G </div>)
+  } else {
+    return (
+      <Row gutter={[16, 16]}>
+        <Col lg={18} xs={24}>
+          <div style={{ width: '100%', padding: '3rem 4rem' }}>
+            <video style={{ width: '100%' }} src={`http://localhost:5000/${videoDetail.filePath}`} controls/>
+  
+            <List.Item actions>
+              <List.Item.Meta
+                avatar={videoDetail.writer.image}
+                title={videoDetail.writer.name}
+                description={videoDetail.description}/>
+            </List.Item>
+          </div>
+        </Col>
+        <Col lg={6} xs={24}>
+          Side Videos
+        </Col>
+      </Row>
+    )
+  }  
+}
+
+export default VideoDetailPage
--- server/routes/video.js
+++ server/routes/video.js
@@ -36,6 +36,15 @@ router.get('/getVideos', (req, res) => {
     })
 })
 
+router.post('/getVideoDetail', (req, res) => {
+  Video.findOne({ "_id" : req.body.videoId })
+    .populate('writer')
+    .exec((err, videoDetail) => {
+      if (err) return res.status(400).send(err)
+      return res.status(200).json({ success: true, videoDetail })
+    })
+})
+
 const upload = multer({ storage: storage }).single("file")
 
 router.post('/uploadfiles', (req, res) => {
